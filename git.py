7f3b18f54db7254a4859cfe4da9aaa886795a1de
saki maki <bezinarevic.s23@htlwienwest.at>
2025-07-16 09:50:11 +0200

    hilfe zu git

---
+++ git.py
@@ -0,0 +1,8 @@
+#git init: zeigt an in welchem Ordner ich gerade bin
+#git add .: es fügt alle Datein aus dem Ordner in so etwas wie einen zwischn stop zwischen dem hinzufügen von den Datein
+#git commit -m "": das fügt die Datein in die Ordner aus dem
+#git push origin "branch name": man sie dan mit diesem prompt in die echte branch hinzufüge und jetzt kann man sie auch in github sehen
+#git pull origin "branch name": das zieht ideen oder änderungen die ich im github gemacht/bekommen habe auch in git wieder rein das git weis/speichert was ich geändert habe
+#git branch "name": erstellt mir eine neue Branch
+#git checkout "name der branch": ich gehe in die branch rein und Arbeite jetzt mit dieser branch
+#git checkout -b "name der branch": das erstellt die branch und geht sofort in die branch rein
